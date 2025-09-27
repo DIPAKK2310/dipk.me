b62934da6b070672e6fd2c5096daaffd62eb5d93
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-27 16:53:31 +0000

    Chore(Dependancy)Add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import ReactLenis from "lenis/react";
+import Navbar from "@/components/common/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
+        <ReactLenis>
+
+        <Navbar/>
         {children}
+        </ReactLenis>
       </body>
     </html>
   );
