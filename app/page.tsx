12719f776d065fcc0b4aae2ea2cffe2de7b6033f
DIPAKK2310 <dipakkhare217@gmail.com>
2025-09-28 11:20:46 +0000

    Style(height&Padding)Add

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/components/common/Container";
 
 export default function page(){
   return(
-    <Container>
+    <Container className="min-h-screen py-16">
       <h1>Hi</h1>
     </Container>
   );
