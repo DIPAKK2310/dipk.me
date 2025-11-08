3885b5fe70e828cdb03e7698d5ffca12e588e393
DIPAKK2310 <dipakkhare217@gmail.com>
2025-11-08 14:32:34 +0000

    Styling(height:Add)

--- components/landing/Model.tsx
+++ components/landing/Model.tsx
@@ -4,8 +4,8 @@ import ComputersCanvas from "@/components/canvas/ComputersCanvas";
 
 const Model = () => {
   return (
-    <div className=" w-full h-full  mx-auto">
-      <div className="w-full h-1/2">
+    <div className=" w-full h-full  mx-auto mb-20">
+      <div className="w-full h-[350px] md:h-[400px] lg:h-[450px]">
         <ComputersCanvas />
       </div>
     </div>
