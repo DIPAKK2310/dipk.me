5c39fc54873bdc77ca9273eb1ee8ba496eed9bb9
DIPAKK2310 <dipakkhare217@gmail.com>
2025-12-22 17:47:22 +0000

    feat:config cta add

--- config/CTA.tsx
+++ config/CTA.tsx
@@ -6,6 +6,7 @@ export const ctaConfig = {
   // CTA button settings
   linkText: 'Book a Free Call',
   calLink: 'ramxcodes/meeting',
+  //https://cal.com/18-dipak-khare-fw1mgg
 
   // Pre-text message
   preText: "Hey,let's talk.",
