0073f1774c41c34d9fba86bfbba0026df0cec6bb
DIPAKK2310 <dipakkhare217@gmail.com>
2025-12-24 18:17:37 +0000

    fix:page.tsx remove model

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Container from "@/components/common/Container";
 import CTA from "@/components/landing/CTA";
 import Github from "@/components/landing/Github";
 import Hero from "@/components/landing/Hero";
-import Model from "@/components/landing/Model";
 import Projects from "@/components/landing/Projects";
 import Blog from "@/components/landing/Blog";
 import Experience from "@/components/landing/Experience";
